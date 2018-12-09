86d3cf4ce06157c209d78b4c0b638e53c2d55995
Tim Stahel <git@swedneck.xyz>
2018-12-09 14:58:59 +0100

    change how the user pill works

--- src/pages/rss.js
+++ src/pages/rss.js
@@ -8,10 +8,10 @@ const RssPage = () => (
             Github Repository: <a href="https://github.com/maubot/rss">maubot/rss</a><br/>
         </p>
         <p>
-            <UserPill userId="@rss:linuxgaming.life"/> is a bot that posts new entries from RSS/atom feeds.</p>
+            <UserPill userId="@rss:linuxgaming.life" img="media.png" /> is a bot that posts new entries from RSS/atom feeds.</p>
         <h4>Adding the bot to your room</h4>
         <ol>
-            <li>Invite <UserPill userId="@rss:linuxgaming.life"/> to your room</li>
+            <li>Invite <UserPill userId="@rss:linuxgaming.life" img="media.png" /> to your room</li>
             <li>Use: Send <code>!rss help</code> to get a list of commands.</li>
 				</ol>
     </div>
